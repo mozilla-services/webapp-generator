06cb5a198d78c7ffb2519e656d5a1c872aacbf38
Alexis Métaireau <alexis@notmyidea.org>
2012-12-10 16:13:28 +0100

    import everything

---
+++ generator/web.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/manifest.webapp')
+def get_manifest():
+    hash = request.host.split('.', 1)[0]
+    return (render_template('manifest.webapp', hash=hash),
+            200, {'Content-Type': 'application/x-web-app-manifest+json'})
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)

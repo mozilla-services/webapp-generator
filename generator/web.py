94ec9f3af2bf1436383273c3cea8cf33ab5cb72b
Alexis Métaireau <alexis@notmyidea.org>
2013-03-04 17:22:29 -0800

    put something at the root

--- generator/web.py
+++ generator/web.py
@@ -10,5 +10,10 @@ def get_manifest():
             200, {'Content-Type': 'application/x-web-app-manifest+json'})
 
 
+@app.route('/')
+def main():
+    return 'yay'
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
